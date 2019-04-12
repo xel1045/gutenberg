0791e4226e54ef6270c22131e4da4a0ace663f60
Andrew Duthie <andrew@andrewduthie.com>
2019-04-12 10:05:43 -0400

    Block Editor: Assign Provider isSyncingOutcomingValue only when blocks changing

--- packages/e2e-tests/specs/undo.test.js
+++ packages/e2e-tests/specs/undo.test.js
@@ -6,6 +6,8 @@ import {
 	getEditedPostContent,
 	createNewPost,
 	pressKeyWithModifier,
+	selectBlockByClientId,
+	getAllBlocks,
 } from '@wordpress/e2e-test-utils';
 
 describe( 'undo', () => {
@@ -62,5 +64,38 @@ describe( 'undo', () => {
 		expect( await getEditedPostContent() ).toBe( '' );
 		// After undoing every action, there should be no more undo history.
 		expect( await page.$( '.editor-history__undo[aria-disabled="true"]' ) ).not.toBeNull();
+	} );
+
+	it( 'should undo for explicit persistence editing post', async () => {
+		// Regression test: An issue had occurred where the creation of an
+		// explicit undo level would interfere with blocks values being synced
+		// correctly to the block editor.
+		//
+		// See: https://github.com/WordPress/gutenberg/issues/14950
+
+		// Issue is demonstrated from an edited post: create, save, and reload.
+		await clickBlockAppender();
+		await page.keyboard.type( 'Hello' );
+		await pressKeyWithModifier( 'primary', 's' );
+		await page.reload();
+
+		// Issue is demonstrated by forcing state merges (multiple inputs) on
+		// an existing text after a fresh reload.
+		await selectBlockByClientId( ( await getAllBlocks() )[ 0 ].clientId );
+		await page.keyboard.press( 'End' );
+		await page.keyboard.type( ' World' );
+
+		// The issue is demonstrated after the one second delay to trigger the
+		// creation of an explicit undo persistence level.
+		await new Promise( ( resolve ) => setTimeout( resolve, 1000 ) );
+
+		await pressKeyWithModifier( 'primary', 'z' );
+
+		// Assert against the _visible_ content. Since editor state with the
+		// regression present was accurate, it would produce the correct
+		// content. The issue had manifested in the form of what was shown to
+		// the user since the blocks state failed to sync to block editor.
+		const visibleContent = await page.evaluate( () => document.activeElement.textContent );
+		expect( visibleContent ).toBe( 'Hello' );
 	} );
 } );
